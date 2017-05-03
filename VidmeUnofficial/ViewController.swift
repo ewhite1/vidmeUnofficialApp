3d72f6ea2decc7cad69c9ca15b56b8ef26cbed81
Edward White <edwardwhite1@outlook.com>
2017-05-03 02:08:44 -0500

    video one merged and completed(pretend I merged it from that branch ;)

--- VidmeUnofficial/ViewController.swift
+++ VidmeUnofficial/ViewController.swift
@@ -15,7 +15,7 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
         // Do any additional setup after loading the view, typically from a nib.
         navigationItem.title = "Home"
         collectionView?.backgroundColor = UIColor.white
-        collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+        collectionView?.register(VideoCell.self, forCellWithReuseIdentifier: "cellId")
     }
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -24,7 +24,7 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
         
-        cell.backgroundColor = UIColor.red
+        
         return cell
     }
     
@@ -32,5 +32,109 @@ class HomeViewController: UICollectionViewController, UICollectionViewDelegateFl
         let size = CGSize(width: view.frame.width, height: 200)
        return size
     }
-}
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    
+    class VideoCell: UICollectionViewCell {
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            setupView()
+        }
+        let thumbnailImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.backgroundColor = UIColor.blue
+            imageView.image = UIImage(named: "")
+        
+            return imageView
+        }()
+        
+        let userProfileImageView: UIImageView = {
+            let imageView = UIImageView()
+            imageView.backgroundColor = UIColor.green
+            return imageView
+        }()
+        
+        
+        let separatorView: UIView = {
+            let view = UIView()
+            view.backgroundColor = UIColor.black
+            
+            return view
+            
+        }()
+        let titleLabel: UILabel = {
+            let label = UILabel()
+            label.backgroundColor = UIColor.purple
+            label.translatesAutoresizingMaskIntoConstraints = false
+            return label
+        }()
+        
+        let subtitleTextView: UITextView = {
+            let textView = UITextView()
+            textView.backgroundColor = UIColor.red
+            textView.translatesAutoresizingMaskIntoConstraints = false
+            return textView
+        }()
+        
+        func setupView(){
+            addSubview(thumbnailImageView)
+            addSubview(separatorView)
+            addSubview(userProfileImageView)
+            addSubview(titleLabel)
+            addSubview(subtitleTextView)
+            
+            //horizontal constraints
+            addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView, separatorView)
+            addConstraintsWithFormat(format: "H:|-16-[v0(44)]", views: userProfileImageView)
+            
+            //vertical constraints
+            addConstraintsWithFormat(format: "V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView, userProfileImageView, separatorView)
+            
+            addConstraintsWithFormat(format: "H:|[v0]|", views: separatorView)
 
+
+           // addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0(1)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": separatorView]))
+            // title label constraints
+            //top constraint
+            addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 8))
+            //left constraint
+            addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: userProfileImageView, attribute: .right, multiplier: 1, constant: 8))
+            // right constraint
+            addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+          
+            //height constraint
+            addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+            // subtitle textview constraints
+            
+            //top constraint
+            addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 8))
+            //left constraint
+            addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .left, relatedBy: .equal, toItem: userProfileImageView, attribute: .right, multiplier: 1, constant: 8))
+            // right constraint
+            addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+            
+            //height constraint
+            addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+         
+        }
+        
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+    }
+    
+   }
+
+extension UIView {
+    func addConstraintsWithFormat(format: String, views: UIView...) {
+        var viewsDictionary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
